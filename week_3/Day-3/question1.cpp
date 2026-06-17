3dea2bc51c052be0c03dcad6613f1d55c7f03784
Babita ray <raybabita957@gmail.com>
2026-06-17 20:18:00 +0530

    questions

---
+++ week_3/Day-3/question1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                stk.push(ch);
+            } else {
+                if (!stk.empty()) {
+                    char top = stk.top();
+
+                    if ((top == '(' && ch == ')') ||
+                        (top == '{' && ch == '}') ||
+                        (top == '[' && ch == ']')) {
+                        stk.pop();
+                    } else {
+                        return false;
+                    }
+
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (stk.empty()) {
+            return true;
+        }
+        return false;
+    }
+};
